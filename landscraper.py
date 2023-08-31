a54759692bc7a6cc85935f23c780af4c0f54ea3e
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-08-30 19:57:09 -0500

    landscaper: user can buy battery-powered lawnmower

--- landscraper.py
+++ landscraper.py
@@ -28,6 +28,21 @@ def buy_push_lawnmower(earnings, has_scissors, has_lawnmower):
     
     return earnings, has_lawnmower
 
+def cut_grass_with_lawnmower(earnings):
+    earnings += 50
+    print("You used the old push lawnmower to cut grass and earned $50.")
+    return earnings
+
+def buy_fancy_lawnmower(earnings, has_lawnmower, has_fancy_lawnmower):
+    if not has_fancy_lawnmower and earnings >= 250 and has_lawnmower:
+        earnings -= 250
+        has_fancy_lawnmower = True
+        print("You bought a fancy lawnmower!")
+    else:
+        print("You either already have a fancy lawnmower, don't have enough money, or need an old lawnmower to buy it.")
+
+    return earnings, has_fancy_lawnmower
+
 def display_earnings(earnings):
     print(f"Total earnings: ${earnings}")
 
@@ -38,11 +53,13 @@ def main():
 
     while True:
         print("1. Use teeth to cut grass and earn $1")
-        print("2. Buy rusty scrissors for $5")
+        print("2. Buy rusty scissors for $5")
         print("3. Use rusty scissors to cut grass and earn $5")
-        print("4. Buy push lawnmower for $25")
-        print("5. Display total earnings")
-        print("6. Quit")
+        print("4. Buy old push lawnmower for $25")
+        print("5. Use lawnmower to cut grass and earn $50")
+        print("6. Buy fancy lawnmower for $250")
+        print("7. Display total earnings")
+        print("8. Quit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -53,12 +70,19 @@ def main():
             if has_scissors:
                 earnings = cut_grass_with_scissors(earnings)
             else:
-                print("You need to buy rusty scissors first")
+                print("You need to buy rusty scissors first.")
         elif choice == "4":
             earnings, has_lawnmower = buy_push_lawnmower(earnings, has_scissors, has_lawnmower)
         elif choice == "5":
-            display_earnings(earnings)
+            if has_lawnmower:
+                earnings = cut_grass_with_lawnmower(earnings)
+            else:
+                print("You need to buy an old-timey push lawnmower first.")
         elif choice == "6":
+            earnings, has_fancy_lawnmower = buy_fancy_lawnmower(earnings, has_lawnmower, has_fancy_lawnmower)
+        elif choice == "7":
+            display_earnings(earnings)
+        elif choice == "8":
             print("Exiting the program.")
             break
         else:
